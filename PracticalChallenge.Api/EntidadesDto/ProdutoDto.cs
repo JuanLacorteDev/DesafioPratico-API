08c570cfe770ddd1c966f875b08c48c619cae654
juan henrique <lacorte07@hotmail.com>
2021-09-03 11:35:19 -0300

    ajuste para request do front

--- PracticalChallenge.Api/EntidadesDto/ProdutoDto.cs
+++ PracticalChallenge.Api/EntidadesDto/ProdutoDto.cs
@@ -1,20 +1,20 @@
-﻿using PracticalChallenge.Business.Mensagens;
+﻿using Newtonsoft.Json;
+using PracticalChallenge.Business.Mensagens;
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace PracticalChallenge.Api.EntidadesDto
 {
     public class ProdutoDto
     {
         [Key]
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public Guid Id { get; set; }
         [Required]
         public string Nome { get; set; }
         [Required]
         public string Quantidade { get; set; }
         [Required]
-        [RegularExpression(@"^-?[0-9][0-9,\.]+$", ErrorMessage = MensagensValidacao.ME003)]
         public decimal ValorUnitario { get; set; }
     }
 }
